339769a5e1ecde1bdd779a08a95a61d57f237010
Rehan Pochkhanawala <rehanp@msn.com>
2017-03-10 05:54:28 +0000

    Comments added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   
+  http_basic_authenticate_with name: "dh", password: "secret", except: [:index, :show]
+  
   def index
     @articles = Article.all
   end
