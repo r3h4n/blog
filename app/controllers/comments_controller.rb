5e813779f4c23db9f5a59d445f71ad2ab4ddbfc0
Rehan Pochkhanawala <rehanp@msn.com>
2017-03-11 19:13:20 +0000

    updated root path

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   
-  http_basic_authenticate_with name: "dh", password: "secret", only: :destroy
+  http_basic_authenticate_with name: "rehan", password: "secret", only: :destroy
 
  def create
     @article = Article.find(params[:article_id])
