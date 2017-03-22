b6284b2a4fe5ba98ab9d3bf4e06502824edb39da
Rehan Pochkhanawala <rehanp@msn.com>
2017-03-22 05:09:36 +0000

    Added AJAX Todo list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320000729) do
+ActiveRecord::Schema.define(version: 20170321184014) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20170320000729) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string   "description"
+    t.string   "priority"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
