11ea660f2ac96d9ea6fa3d29bfca2c4f96d671ce
Rehan Pochkhanawala <rehanp@msn.com>
2017-03-25 19:52:43 +0000

    ActionCable chat comments

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Blog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  config.assets.initialize_on_precompile = false
-
+  # config.assets.initialize_on_precompile = false
+  config.action_cable.mount_path = "/cable"
   end
 end
