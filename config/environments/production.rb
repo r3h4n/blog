1a28fa299346d461ef8c6c08e63230347a24cea0
Rehan Pochkhanawala <rehanp@msn.com>
2017-03-26 04:30:04 +0000

    Make app production ready with a ction cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://rehan-blog.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['http://rehan-blog.herokuapp.com', 'http://rehan-blog.herokuapp.com']
 end
