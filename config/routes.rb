86061eaa90fbe7b478782bc3acc2ebd08ee9d81f
Rehan Pochkhanawala <rehanp@msn.com>
2017-03-27 02:29:39 +0000

    Complete chatroom feature

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,9 @@ Rails.application.routes.draw do
   delete '/logout', to: "sessions#destroy"
 
   mount ActionCable.server => '/cable'
+  get '/chat', to: 'chatrooms#show'
+  
+  resources :messages, only: [:create]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325205414) do
+ActiveRecord::Schema.define(version: 20170326235431) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20170325205414) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
