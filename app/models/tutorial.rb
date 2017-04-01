4acf1604bc31d14f241342b90300f6f20a3edba4
Rehan Pochkhanawala <rehanp@msn.com>
2017-04-01 22:29:50 +0000

    CSS tweaks

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -7,7 +7,8 @@ class Tutorial < ApplicationRecord
   validates :author, presence: true
   validates :tutorial_img, presence: true
   validates :category_id, presence: true
-
+  
+## add  :bucket => 'S3_BUCKET_NAME' to attached_file for development envioronment test
   has_attached_file :tutorial_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :tutorial_img, content_type: /\Aimage\/.*\z/
 
