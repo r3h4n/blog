63a251310986d2c2f25efdc025e77f2c16007538
Rehan Pochkhanawala <rehanp@msn.com>
2017-04-01 19:40:56 +0000

    Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,13 +89,13 @@ Rails.application.configure do
 # Amazon S3 Buckets
 
   config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION')
-    }
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
+}
 
 end
