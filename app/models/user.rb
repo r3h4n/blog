1495e2083bb9963b4c8baa9f91ed92c74369a904
Rehan Pochkhanawala <rehanp@msn.com>
2017-04-04 22:41:41 +0000

    bucket removal

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
 
   
   ## add  :bucket => 'S3_BUCKET_NAME' to attached_file for development envioronment test
-   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   
