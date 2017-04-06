057de22bb46190f45af6ddccbe43cdfaa3d847b0
Rehan Pochkhanawala <rehanp@msn.com>
2017-04-06 21:55:33 +0000

    production rdy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,42 +1,4 @@
 Rails.application.configure do
-<<<<<<< HEAD
-  
-  config.cache_classes = true
-  
-  config.eager_load = true
-  
-  config.consider_all_requests_local = false
-  config.action_controller.perform_caching = true
-  
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  
-  config.assets.js_compressor = :uglifier
-  
-  config.assets.compile = false
-  
-  config.log_level = :debug
-  
-  config.log_tags = [ :request_id]
-  
-  config.action_mailer.perform_caching = false
-  
-  config.i18n.fallbacks = true
-  
-  config.active_support.deprecation = :notify
-  
-  config.log_formatter = ::Logger::Formatter.new
-  
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger  = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = cnonfig.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
-  
-  config.active_record.dump_schema_after_migration = false
-  config.web_socket_server_url = "wss://rehan-blog.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['http://rehan-blog.herokuapp.com', 'http://rehan-blog.herokuapp.com']
-end
-=======
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -122,10 +84,9 @@ end
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.web_socket_server_url = "wss://rehan-blog.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://rehan-blog.herokuapp.com', 'https://rehan-blog.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['http://rehan-blog.herokuapp.com', 'http://rehan-blog.herokuapp.com']
 
 # Amazon S3 Buckets
 
  
 end
->>>>>>> 3fbe1046d29e92ab73039366db81cab52d36d264
